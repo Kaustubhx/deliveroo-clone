d825b20bdc322e99e871820ae5c07f8c194b27f2
Kastubhx <prime.ganekar@gmail.com>
2022-08-20 22:21:26 +0530

    Query & Pull: Sanity backend

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import { View, Text, Image, TextInput, ScrollView, } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {
@@ -9,16 +9,41 @@ import {
     AdjustmentsIcon,
 } from "react-native-heroicons/outline";
 import Categories from '../components/Categories';
+import FeaturedRow from '../components/FeaturedRow';
+import client from '../sanity'
 
 const HomeScreen = () => {
-
     const navigation = useNavigation();
+    const [featuredCategories, setFeaturedCategories] = useState([]);
+
 
     useLayoutEffect(() => {
         navigation.setOptions({
             headerShown: false,
         })
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        client
+            .fetch(
+                `
+                *[_type == "featured"] {
+                    ...,
+                    restaurants[]-> {
+                        ...,
+                        dishes[]-> {
+                            ...,
+                        }
+                    }
+                }
+            `
+            )
+            .then((data) => {
+                setFeaturedCategories(data);
+            });
+    }, []);
+
+    // console.log(featuredCategories);
 
     return (
         <SafeAreaView className='bg-white pt-5'>
@@ -45,7 +70,7 @@ const HomeScreen = () => {
             {/* Search */}
             <View className='flex-row items-center space-x-2 pb-2 mx-4'>
                 <View className='flex-row space-x-2 flex-1 bg-gray-200 p-3'>
-                    <SearchIcon size={20}  color='#00CCBB'/>
+                    <SearchIcon size={20} color='#00CCBB' />
                     <TextInput
                         placeholder='Restaurants and Cusine'
                         keyboardType='default'
@@ -55,14 +80,29 @@ const HomeScreen = () => {
                 <AdjustmentsIcon color='#00CCBB' />
             </View>
 
-            {/* Body */}
-            <ScrollView>
-                {/* Category */}
-                <Categories />
 
-                {/* Featured Row */}
-            </ScrollView>
-        </SafeAreaView>
+            {/* Body */}
+            <ScrollView
+                contentContainerStyle={{
+                    paddingBottom: 120
+                }}
+                className='bg-gray-100'
+            >
+            {/* Category */}
+            <Categories />
+
+            {/* Featured Row */}
+
+            {featuredCategories.map(category => (
+                <FeaturedRow
+                    key={category._id}
+                    id={category._id}
+                    title={category.name}
+                    description={category.short_description}
+                />
+            ))}
+        </ScrollView>
+        </SafeAreaView >
     )
 }
 
